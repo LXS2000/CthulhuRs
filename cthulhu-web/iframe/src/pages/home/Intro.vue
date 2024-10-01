4b1cb1a01cf72f315753ea829ee3ee5e4f9bb0ac
LXS <3451743380@qq.com>
2024-10-01 14:12:57 +0800

    optimize

--- cthulhu-web/iframe/src/pages/home/Intro.vue
+++ cthulhu-web/iframe/src/pages/home/Intro.vue
@@ -5,7 +5,7 @@
             allow="clipboard-write"
             frameborder="0"
             style="width: 100%;height: 100%"
-            :src="'https://doc.cthulhu.server/zh/'"/>
+            src="https://lxs2000.github.io/cthulhurs-doc/dist/en/"/>
   </div>
 
 </template>
